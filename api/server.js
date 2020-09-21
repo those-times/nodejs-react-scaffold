9d392bcfcb30020fe8ac4bfd614cb8d3e42099eb
Volodymyr Yanov <vlad.yanov@gmail.com>
2020-09-21 13:30:09 -0700

    Update server.js

--- api/server.js
+++ api/server.js
@@ -7,4 +7,4 @@ app.use(cors())
 const port = 5000
 
 app.get('/', (req, res) => res.json({ text: 'Hello World!' }));
-app.listen(port, () => console.log(`Sequelize Demo app listening on port ${port}!`))
+app.listen(port, () => console.log(`App listening on port ${port}!`))
